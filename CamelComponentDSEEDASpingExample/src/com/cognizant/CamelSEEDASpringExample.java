ff2048878b31af419ddf314944245c0311bc67ae
Sankalp Saxena <sankalp.saxena@entorcommerce.com>
2017-01-06 21:17:45 +0530

    test

--- CamelComponentDSEEDASpingExample/src/com/cognizant/CamelSEEDASpringExample.java
+++ CamelComponentDSEEDASpingExample/src/com/cognizant/CamelSEEDASpringExample.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CamelSEEDASpringExample{
+	
 	public static final void main(String[] args) throws Exception {
 		ApplicationContext appContext = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
