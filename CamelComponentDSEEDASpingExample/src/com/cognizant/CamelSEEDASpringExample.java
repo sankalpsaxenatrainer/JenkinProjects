e1f27e87fd5291befec556fcfa46ebf5482026da
sankalpsaxenatrainer <sankalpsaxenatrainer@gmail.com>
2017-01-06 21:47:59 +0530

    Update CamelSEEDASpringExample.java

--- CamelComponentDSEEDASpingExample/src/com/cognizant/CamelSEEDASpringExample.java
+++ CamelComponentDSEEDASpingExample/src/com/cognizant/CamelSEEDASpringExample.java
@@ -5,6 +5,7 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+ 
 
 public class CamelSEEDASpringExample{
 	
