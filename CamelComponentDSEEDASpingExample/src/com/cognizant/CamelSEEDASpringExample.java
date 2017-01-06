ecabb45452e9de1dcbe98d834d486fd3f9a78c0d
Sankalp Saxena <sankalp.saxena@entorcommerce.com>
2017-01-06 20:26:21 +0530

    Testing

---
+++ CamelComponentDSEEDASpingExample/src/com/cognizant/CamelSEEDASpringExample.java
@@ -0,0 +1,24 @@
+package com.cognizant;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CamelSEEDASpringExample{
+	public static final void main(String[] args) throws Exception {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+		CamelContext camelContext = SpringCamelContext.springCamelContext(
+				appContext, false);
+		try {
+			ProducerTemplate template = camelContext.createProducerTemplate();
+			camelContext.start();
+			Object out = template.requestBody("activemq:queue:test.queue", "Seda Example");
+			System.out.println("Response: " + out);
+		} finally {
+			camelContext.stop();
+		}
+	}
+}
---
+++ CamelComponentDSEEDASpingExample/src/com/cognizant/TextMessageBean.java
@@ -0,0 +1,19 @@
+package com.cognizant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextMessageBean {
+
+	private List messages = new ArrayList();
+
+	public String textMethod(String msg) {
+		String textMessageBean = "Training Bean Direct Example Says " + msg;
+		messages.add(textMessageBean);
+		return textMessageBean;
+	}
+
+	public String toString() {
+		return messages.toString();
+	}
+}
